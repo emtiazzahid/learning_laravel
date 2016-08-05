65413630106fa248008ecfa7b819b88598119986
emtiazzahid <emtiazzahid@gmail.com>
2016-08-06 02:24:55 +0600

    learned about routing, page creating and introduced to views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+
+@section('content')
+	<div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat sunt asperiores enim, id modi numquam vitae dolorem perspiciatis mollitia, excepturi a labore. Eligendi maiores ab autem non. Et, facilis, pariatur!</p>
+	</div>
+@endsection
