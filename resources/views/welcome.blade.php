ccb1af20903f2fd3577b3456192e6838efd3ec58
emtiazzahid <emtiazzahid@gmail.com>
2016-08-06 01:51:45 +0600

    laravel full project files added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+          
+        </style>
+    </head>
+    <body>
+       <h1>Hi i am zahid</h1>
+       <p>this is my first laravel app</p>
+    </body>
+</html>
